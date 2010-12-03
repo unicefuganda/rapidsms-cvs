c23b76211d815e24826ef23a607d77ce17133d89
David McCann <david.a.mccann@gmail.com>
2010-12-03 11:50:55 +0300

    impl of healthmodel extensions

---
+++ cvs/extensions/healthmodels/patientreport.py
@@ -0,0 +1,13 @@
+from django.db import models
+from rapidsms_xforms.models import XFormSubmission
+from healthmodels.models.HealthProvider import HealthProvider
+from rapidsms_httprouter.models import Message
+
+class XFormPatientReport(models.Model):
+    """
+    FIXME: Documentation
+    """
+    submission = models.ForeignKey(XFormSubmission, null=True, blank=True)
+    reporter = models.ForeignKey(HealthProvider, null=True, blank=True)
+    message = models.ForeignKey(Message, null=True, blank=True)
+    valid = models.BooleanField(default=True)
