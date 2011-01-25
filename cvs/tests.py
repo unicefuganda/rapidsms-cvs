f308653c16b505bb2f715e8034f29b2ec41c9086
David McCann <david.a.mccann@gmail.com>
2011-01-25 12:44:32 +0300

    basic birth test case

--- cvs/tests.py
+++ cvs/tests.py
@@ -40,13 +40,33 @@ class ModelTest(TestCase): #pragma: no cover
             submission = form.process_sms_submission(incomingmessage)
 
     def testBasicSubmission(self):
-        self.fakeIncoming('+BIRTH ALIMO MARY, F, HOME')
-        self.fakeIncoming('+DEATH ADOCH,F,1DAY')
-        self.fakeIncoming('+muac auma gloria,f,5months,yellow')
+        self.fakeIncoming('+BIRTH Terra Weikel, F, HOME')
+        self.fakeIncoming('+DEATH Malthe Borch, M,1DAY')
+        self.fakeIncoming('+muac Sean Blaschke, M,5months,yellow')
         self.fakeIncoming('+epi ma 12, bd 5')
         self.fakeIncoming('+home 12, wa 1, it 6')
         self.assertEquals(XFormSubmission.objects.count(), 5)
         self.assertEquals(PatientEncounter.objects.count(), 3)
+
+    def testBirth(self):
+        self.fakeIncoming('+birth David McCann, M, Facility')
+        # test submission parsing
+        s = XFormSubmission.objects.all()[0]
+        self.assertEquals(s.eav.birth_place, 'FACILITY')
+        self.assertEquals(s.eav.birth_name, 'David McCann')
+        self.assertEquals(s.eav.birth_gender, 'M')
+        self.assertEquals(PatientEncounter.objects.filter(submission=s).count(), 1)
+        pe = PatientEncounter.objects.get(submission=s)
+        p = pe.patient
+        # test patient creation
+        self.assertEquals(p.first_name, 'David')
+        self.assertEquals(p.last_name, 'McCann')
+        self.failUnless(p.age.days < 1)
+        self.fakeIncoming('+birth terra weikel, F, Home')
+        self.assertEquals(XFormSubmission.objects.order_by('-pk')[0].eav.birth_place, 'HOME')
+        self.assertEquals(XFormSubmission.objects.order_by('-pk')[0].eav.birth_gender, 'F')
+
+
 
     def testSimpleDocTest(self):
         """
