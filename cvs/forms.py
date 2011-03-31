32865929caa27a869b256c95bf2840a48feb97ac
David McCann <david.a.mccann@gmail.com>
2011-03-31 15:15:28 +0300

    params are url parameters that should be passed to reverse

--- cvs/forms.py
+++ cvs/forms.py
@@ -73,13 +73,13 @@ class ChartModuleForm(ModuleForm):
     def setModuleParams(self, dashboard, module=None):
         module = module or self.createModule(dashboard)
         module.view_name = 'cvs.views.chart.chart'
-        module.params.create(module=module, param_name='location_id', param_value=str(self.cleaned_data['district'].pk))
+        module.params.create(module=module, param_name='location_id', param_value=str(self.cleaned_data['district'].pk), is_url_param=True)
         param_list = self.cleaned_data['type']
         if len(param_list) > 0:
-            module.params.create(module=module, param_name='xform_keyword', param_value=param_list[0])
+            module.params.create(module=module, param_name='xform_keyword', param_value=param_list[0], is_url_param=True)
         if len(param_list) > 1:
-            module.params.create(module=module, param_name='attribute_keyword', param_value=param_list[1])
+            module.params.create(module=module, param_name='attribute_keyword', param_value=param_list[1], is_url_param=True)
         if len(param_list) > 2:
-            module.params.create(module=module, param_name='attribute_value', param_value=param_list[2])
+            module.params.create(module=module, param_name='attribute_value', param_value=param_list[2], is_url_param=True)
 
         return module
