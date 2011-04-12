5f4b698052e0d9923653c2e8202cba2cc67c34ab
Mugisha Moses <moses@appfrica.org>
2011-04-12 12:58:07 +0300

    re-organised dependancy links

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     ],
 
     dependency_links = [
+        "http://github.com/daveycrockett/code_generator/tarball/master#egg=code_generator",
+        "http://github.com/mossplix/simple_locations/tarball/master#egg=simple_locations",
         "http://github.com/daveycrockett/healthmodels/tarball/master#egg=healthmodels",
         "http://github.com/daveycrockett/rapidsms-xforms/tarball/master#egg=rapidsms-xforms",
         "http://github.com/daveycrockett/auth/tarball/master#egg=rapidsms-auth",
@@ -30,8 +32,6 @@ setup(
         "http://github.com/daveycrockett/rapidsms-httprouter/tarball/master#egg=rapidsms-httprouter",
         "http://github.com/daveycrockett/rapidsms-ureport/tarball/master#egg=rapidsms-ureport",
         "http://github.com/mvpdev/django-eav/tarball/master#egg=django-eav",
-        "http://github.com/mossplix/simple_locations/tarball/master#egg=simple_locations",
-        "http://github.com/daveycrockett/code_generator/tarball/master#egg=code_generator",
         "http://github.com/daveycrockett/rapidsms-auth/tarball/master#egg=rapidsms-auth",
         "http://github.com/daveycrockett/rapidsms-authsites/tarball/master#egg=rapidsms-authsites",
     ],
