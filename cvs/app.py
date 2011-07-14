4d5d5d58a2a876115905dd369c68430eb0c89555
assey alfred <asseym@gmail.com>
2011-07-14 12:03:20 +0300

    added cvs auto registration

--- cvs/app.py
+++ cvs/app.py
@@ -1,31 +1,7 @@
-#<<<<<<< HEAD
-#'''
-#Created on Jul 12, 2011
-#
-#@author: asseym
-#'''
-#from rapidsms.apps.base import AppBase
-#from script.models import *
-#
-#class App (AppBase):
-#    '''
-#    classdocs
-#    '''
-#
-#    def handle (self, message):
-#        if not message.connection.contact:
-#            message.connection.contact = Contact.objects.create(name='Anonymous User')
-#            message.connection.save()
-#            ScriptProgress.objects.create(script=Script.objects.get(slug="cvs_autoreg"),\
-#                                          connection=message.connection)
-#            return True
-#        return False
-#
-#        
-#=======
 from rapidsms.models import Contact
 from django.conf import settings
 from rapidsms.apps.base import AppBase
+from script.models import ScriptProgress
 
 class App (AppBase):
     
@@ -39,5 +15,9 @@ class App (AppBase):
                 message.connection.contact.save()
                 message.respond(getattr(settings,'ACTIVATION_MESSAGE','Congratulations, you are now active in the system!'))
                 return True
+        elif not message.connection.contact:
+            message.connection.contact == Contact.objects.create(name='Annoymous User')
+            message.connection.save()
+            ScriptProgress.objects.create(script=Script.objects.get(slug="cvs_autoreg"),connection=message.connection)
+            return True
         return False
-#>>>>>>> 9aff6f0010d7b0185318ff8ba42264c6f9aa78fc
