4fd2cf85a1485dd6b2181673c07c1c7f43f72d2b
assey alfred <asseym@gmail.com>
2011-07-15 10:54:41 +0300

    minor bug fix

--- cvs/app.py
+++ cvs/app.py
@@ -1,7 +1,7 @@
 from rapidsms.models import Contact
 from django.conf import settings
 from rapidsms.apps.base import AppBase
-from script.models import ScriptProgress
+from script.models import ScriptProgress, Script
 
 class App (AppBase):
     
@@ -16,7 +16,7 @@ class App (AppBase):
                 message.respond(getattr(settings,'ACTIVATION_MESSAGE','Congratulations, you are now active in the system!'))
                 return True
         elif not message.connection.contact:
-            message.connection.contact == Contact.objects.create(name='Annoymous User')
+            message.connection.contact = Contact.objects.create(name='Anonymous User')
             message.connection.save()
             ScriptProgress.objects.create(script=Script.objects.get(slug="cvs_autoreg"),connection=message.connection)
             return True
