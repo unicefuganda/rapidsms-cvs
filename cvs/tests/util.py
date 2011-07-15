2039384dcbcda6104400a518b496e97ea83f538e
Rowena <rluk@dimagi.com>
2011-07-15 10:22:40 +0300

    create connections and backends automatically if they don't already exist

--- cvs/tests/util.py
+++ cvs/tests/util.py
@@ -1,16 +1,20 @@
-from rapidsms.models import Connection
+from rapidsms.models import Connection, Backend
 from rapidsms_httprouter.models import Message
 from rapidsms.messages.incoming import IncomingMessage
 from rapidsms_xforms.models import XForm
 
 def fake_incoming(message, connection=None):
     form = XForm.find_form(message)
-
     if connection is None:
-        connection = Connection.objects.all()[0]
+        try:
+            connection = Connection.objects.all()[0]
+        except IndexError:
+            backend, created = Backend.objects.get_or_create(name='test')
+            connection, created = Connection.objects.get_or_create(identity='8675309', 
+                                                                   backend=backend)
     # if so, process it
     incomingmessage = IncomingMessage(connection, message)
-    incomingmessage.db_message = Message.objects.create(direction='I', connection=Connection.objects.all()[0], text=message)
+    incomingmessage.db_message = Message.objects.create(direction='I', connection=connection, text=message)
     if form:
         submission = form.process_sms_submission(incomingmessage)
         return submission
