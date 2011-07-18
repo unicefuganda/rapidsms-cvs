8f87d0e773db4465583bba8860eaf8bd41eb8e06
David McCann <david.a.mccann@gmail.com>
2011-07-18 19:50:15 +0300

    form fix

--- cvs/forms.py
+++ cvs/forms.py
@@ -39,7 +39,7 @@ class FacilityFilterForm(FilterForm):
     facility=forms.ChoiceField(choices=())
     
     def __init__(self, data=None, **kwargs):
-        response = kwargs.pop('response')
+        response = kwargs.pop('request')
         if data:
             forms.Form.__init__(self, data, **kwargs)
         else:
