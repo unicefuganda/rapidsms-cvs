6d47d4398fd078478f007b66f5a79acc69d9094e
David McCann <david.a.mccann@gmail.com>
2011-08-02 06:00:50 +0300

    bug fix to filter by facility

--- cvs/forms.py
+++ cvs/forms.py
@@ -6,7 +6,7 @@ from healthmodels.models.HealthFacility import HealthFacility
 from mptt.forms import TreeNodeChoiceField
 from rapidsms.contrib.locations.models import Location
 
-date_range_choices=(('w','Previous Calendar Week'),('m','Previous Calendar Month'),('q','Previous calendar quarter'),)
+date_range_choices = (('w', 'Previous Calendar Week'), ('m', 'Previous Calendar Month'), ('q', 'Previous calendar quarter'),)
 class DateRangeForm(forms.Form): # pragma: no cover
     start_ts = forms.IntegerField(required=True, widget=forms.HiddenInput())
     end_ts = forms.IntegerField(required=True, widget=forms.HiddenInput())
@@ -28,23 +28,16 @@ class EditReporterForm(forms.ModelForm):
         global AREAS
         super(EditReporterForm, self).__init__(*args, **kwargs)
         len(AREAS)
-        self.fields['location'] = TreeNodeChoiceField(queryset=AREAS, level_indicator=u'.',required=False, empty_label='----')
+        self.fields['location'] = TreeNodeChoiceField(queryset=AREAS, level_indicator=u'.', required=False, empty_label='----')
 
     class Meta:
-        model=HealthProvider
+        model = HealthProvider
         fields = ('name', 'facility', 'location')
 
 class FacilityFilterForm(FilterForm):
     """ filter form for cvs facilities """
-    facility=forms.ChoiceField(choices=())
-    
-    def __init__(self, data=None, **kwargs):
-        response = kwargs.pop('request')
-        if data:
-            forms.Form.__init__(self, data, **kwargs)
-        else:
-            forms.Form.__init__(self, **kwargs)
-        self.fields['facility'].widget.choices = (('','-----'),(-1,'Has No Facility'),)+tuple(HealthFacility.objects.values_list('pk','name').order_by('type','name'))
+    facility = forms.ChoiceField(choices=(('', '-----'), (-1, 'Has No Facility'),) + tuple(HealthFacility.objects.values_list('pk', 'name').order_by('type', 'name')))
+
 
     def filter(self, request, queryset):
         facility_pk = self.cleaned_data['facility']
@@ -66,21 +59,21 @@ class FacilityResponseForm(forms.Form):
     value = forms.ModelChoiceField(queryset=HealthFacility.objects.order_by('name'))
 
 class ChartModuleForm(ModuleForm):
-    type=forms.ChoiceField(choices=(
-        ('epi___ma','Variation of Malaria reports'),
-        ('epi___tb','Variation of Tuberculosis reports'),
-        ('epi___bd','Variation of Bloody Diarrhea reports'),
-        ('muac','Variation of Malnutrition reports'),
-        ('birth','Variation of Birth reports'),
-        ('birth___gender___M','Variation of male birth reports'),
-        ('birth___gender___F','Variation of female birth reports'),
-        ('death','Variation of Death reports'),
+    type = forms.ChoiceField(choices=(
+        ('epi___ma', 'Variation of Malaria reports'),
+        ('epi___tb', 'Variation of Tuberculosis reports'),
+        ('epi___bd', 'Variation of Bloody Diarrhea reports'),
+        ('muac', 'Variation of Malnutrition reports'),
+        ('birth', 'Variation of Birth reports'),
+        ('birth___gender___M', 'Variation of male birth reports'),
+        ('birth___gender___F', 'Variation of female birth reports'),
+        ('death', 'Variation of Death reports'),
         ('home___wa___percentage', 'Variation of Homesteads with safe drinking water'),
         ('epi___percentage', 'Variation of percentage of expected weekly epi reports received'),
     ), label="Data to chart")
     title = forms.CharField(max_length=40)
     district = forms.ModelChoiceField(queryset=Location.objects.filter(type__slug='district').order_by('name'))
-    range=forms.ChoiceField(choices= date_range_choices,label="date Range")
+    range = forms.ChoiceField(choices=date_range_choices, label="date Range")
 
     def setModuleParams(self, dashboard, module=None, title=None):
         module = module or self.createModule(dashboard, 'cvs.views.chart.chart', title=title)
@@ -110,12 +103,12 @@ class MapModuleForm(ModuleForm):
     ('epi__tb', 'TuberClosis'),
     ('epi__tb', 'TuberClosis'),
     ), label="Layers To Map ")
-    title=forms.CharField(max_length=40)
-    range=forms.ChoiceField(choices= date_range_choices,label="date Range")
-    def setModuleParams(self, dashboard, module=None,title=None):
-        if len(self.cleaned_data['title'])>0:
-            title=self.cleaned_data['title']
-        module = module or self.createModule(dashboard, 'cvs.views.map.map_index',title=title)
+    title = forms.CharField(max_length=40)
+    range = forms.ChoiceField(choices=date_range_choices, label="date Range")
+    def setModuleParams(self, dashboard, module=None, title=None):
+        if len(self.cleaned_data['title']) > 0:
+            title = self.cleaned_data['title']
+        module = module or self.createModule(dashboard, 'cvs.views.map.map_index', title=title)
         module.params.create(module=module, param_name='date_range', param_value=str(self.cleaned_data['range']), is_url_param=False)
         module.params.create(module=module, param_name='range', param_value=str(self.cleaned_data['range']), is_url_param=False)
         param_list = self.cleaned_data['layer'].split('__')
@@ -126,13 +119,13 @@ class MapModuleForm(ModuleForm):
 
         return module
 
-    
+
 class StatsModuleForm(ModuleForm):
-    type=forms.ChoiceField(choices=(
-        ('epi_detail','Variation of EPI reports'),
-        ('muac_detail','Variation of Malnutrition reports'),
-        ('birth_detail','Variation of Birth reports'),
-        ('death_detail','Variation of Death reports'),
+    type = forms.ChoiceField(choices=(
+        ('epi_detail', 'Variation of EPI reports'),
+        ('muac_detail', 'Variation of Malnutrition reports'),
+        ('birth_detail', 'Variation of Birth reports'),
+        ('death_detail', 'Variation of Death reports'),
         ('home_detail', 'Variation of Homesteads reports'),
     ), label="Statistics to Show")
     title = forms.CharField(max_length=40)
@@ -142,7 +135,7 @@ class StatsModuleForm(ModuleForm):
                                  d.name) for d in
                                  Location.objects.filter(type__slug='district'
                                  ).order_by('name')]))
-    range=forms.ChoiceField(choices= date_range_choices,label="date Range")
+    range = forms.ChoiceField(choices=date_range_choices, label="date Range")
     def setModuleParams(self, dashboard, module=None, title=None):
         module = module or self.createModule(dashboard, 'cvs.views.stats.module_stats', title=title)
         module.params.create(module=module, param_name='date_range', param_value=str(self.cleaned_data['range']), is_url_param=False)
