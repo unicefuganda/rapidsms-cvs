5242b3deef4d5c962d44716a20fdbef4eeb98bda
David McCann <david.a.mccann@gmail.com>
2011-08-10 14:32:31 +0300

    formatting changes, minor fix to getting dates (in situations where there are currently no submissions

--- cvs/views/dates.py
+++ cvs/views/dates.py
@@ -25,14 +25,14 @@ def get_dates(request):
         if form.is_valid():
             cursor = connection.cursor()
             cursor.execute("select min(created) from rapidsms_xforms_xformsubmission")
-            min_date = cursor.fetchone()[0]
+            min_date = cursor.fetchone()[0] or (datetime.datetime.now() - datetime.timedelta(365))
             start_date = form.cleaned_data['start_ts']
             end_date = form.cleaned_data['end_ts']
             request.session['start_date'] = start_date
             request.session['end_date'] = end_date
-    elif request.GET.get('start_date',None) and request.GET.get('end_date',None):
-        start_date=datetime.datetime.fromtimestamp(int(request.GET['start_date']))
-        end_date=datetime.datetime.fromtimestamp(int(request.GET['end_date']))
+    elif request.GET.get('start_date', None) and request.GET.get('end_date', None):
+        start_date = datetime.datetime.fromtimestamp(int(request.GET['start_date']))
+        end_date = datetime.datetime.fromtimestamp(int(request.GET['end_date']))
         request.session['start_date'] = start_date
         request.session['end_date'] = end_date
         return {'start':start_date, 'end':end_date}
@@ -41,12 +41,14 @@ def get_dates(request):
         cursor = connection.cursor()
         cursor.execute("select min(created), max(created) from rapidsms_xforms_xformsubmission")
         min_date, end_date = cursor.fetchone()
+        end_date = end_date or datetime.datetime.now()
+        min_date = min_date or (datetime.datetime.now() - datetime.timedelta(365))
         start_date = end_date - datetime.timedelta(days=30)
-        if request.GET.get('date_range',None):
-            start_date,end_date=TIME_RANGES[request.GET.get('date_range')]()
-            request.session['start_date'],request.session['end_date']=start_date,end_date
-        if request.session.get('start_date',None)  and request.session.get('end_date',None):
-            start_date=request.session['start_date']
-            end_date=request.session['end_date']
+        if request.GET.get('date_range', None):
+            start_date, end_date = TIME_RANGES[request.GET.get('date_range')]()
+            request.session['start_date'], request.session['end_date'] = start_date, end_date
+        if request.session.get('start_date', None)  and request.session.get('end_date', None):
+            start_date = request.session['start_date']
+            end_date = request.session['end_date']
 
     return {'start':start_date, 'end':end_date, 'min':min_date, 'form':form}
