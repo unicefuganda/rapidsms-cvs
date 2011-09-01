9d44daeac01fcf9ecf909960d012532f2c0a2bdb
David McCann <david.a.mccann@gmail.com>
2011-09-01 12:18:59 +0300

    [Fixes #17598855]

--- cvs/templatetags/stats_extras.py
+++ cvs/templatetags/stats_extras.py
@@ -35,7 +35,7 @@ def get_ancestors(location_id):
 
 def get_district(location):
     try:
-        return location.get_ancestors().get(kind__name='district').name
+        return location.get_ancestors().get(type__name='district').name
     except:
         return None
 
@@ -50,9 +50,9 @@ def latest(obj):
 
 def hash(h, key):
     try:
-        val =h[key]
+        val = h[key]
     except KeyError:
-        val=None
+        val = None
     return val
 month_options = (
     (),
@@ -72,23 +72,23 @@ month_options = (
 
 class DateRangeNode(template.Node):
 
-    def __init__(self ,min_date,max_date,start_date, end_date):
+    def __init__(self , min_date, max_date, start_date, end_date):
         self.end_date = template.Variable(end_date)
         self.start_date = template.Variable(start_date)
         self.min_date = template.Variable(min_date)
-        self.max_date=template.Variable(max_date)
-    def render(self,context):
+        self.max_date = template.Variable(max_date)
+    def render(self, context):
         try:
-            end_date=self.end_date.resolve(context)
-            start_date=self.start_date.resolve(context)
-            min_date=self.min_date.resolve(context)
-            max_date=self.max_date.resolve(context)
+            end_date = self.end_date.resolve(context)
+            start_date = self.start_date.resolve(context)
+            min_date = self.min_date.resolve(context)
+            max_date = self.max_date.resolve(context)
         except template.VariableDoesNotExist:
             return ''
-        start_date=datetime.datetime.fromtimestamp(start_date/1000)
-        end_date=datetime.datetime.fromtimestamp(end_date/1000)
-        min_date=datetime.datetime.fromtimestamp(min_date/1000)
-        max_date=datetime.datetime.fromtimestamp(max_date/1000)
+        start_date = datetime.datetime.fromtimestamp(start_date / 1000)
+        end_date = datetime.datetime.fromtimestamp(end_date / 1000)
+        min_date = datetime.datetime.fromtimestamp(min_date / 1000)
+        max_date = datetime.datetime.fromtimestamp(max_date / 1000)
 
         years = range(min_date.year, max_date.year + 1)
         start_opts = \
@@ -99,48 +99,48 @@ class DateRangeNode(template.Node):
 
             opt_year = "<optgroup label='%s'>" % str(year)
             start_opts = start_opts + opt_year
-            if year==min_date.year:
-                for month in range(min_date.month,13):
+            if year == min_date.year:
+                for month in range(min_date.month, 13):
                     opt_month = "<optgroup label='%s'>" % str(month_options[month][1])
                     start_opts = start_opts + opt_month
-                    for day in range(1,calendar.monthrange(year,month)[1]+1):
+                    for day in range(1, calendar.monthrange(year, month)[1] + 1):
                         option = "<option value=%d>%s-%s-%s</option>"\
-                     % (time.mktime(datetime.datetime(year,month,day).timetuple())*1000, str(day),str(month_options[month][1]),str(year))
+                     % (time.mktime(datetime.datetime(year, month, day).timetuple())*1000, str(day), str(month_options[month][1]), str(year))
                         start_opts = start_opts + option
                     start_opts = start_opts + '</optgroup>'
 
 
-            elif year==max_date.year:
-                for month in range(1,max_date.month+1):
+            elif year == max_date.year:
+                for month in range(1, max_date.month + 1):
                     opt_month = "<optgroup label='%s'>" % str(month_options[month][1])
                     start_opts = start_opts + opt_month
-                    for day in range(1,calendar.monthrange(year,month)[1]+1):
+                    for day in range(1, calendar.monthrange(year, month)[1] + 1):
                         option = "<option value=%d>%s-%s-%s</option>"\
-                     % (time.mktime(datetime.datetime(year,month,day).timetuple())*1000, str(day),str(month_options[month][1]),str(year))
+                     % (time.mktime(datetime.datetime(year, month, day).timetuple())*1000, str(day), str(month_options[month][1]), str(year))
                         start_opts = start_opts + option
                     start_opts = start_opts + '</optgroup>'
             else:
                 for month in range(1, 13):
                     opt_month = "<optgroup label='%s'>" % str(month_options[month][1])
                     start_opts = start_opts + opt_month
-                    for day in range(1,calendar.monthrange(year,month)[1]+1):
+                    for day in range(1, calendar.monthrange(year, month)[1] + 1):
                         option = "<option value=%d>%s-%s-%s</option>"\
-                     % (time.mktime(datetime.datetime(year,month,day).timetuple())*1000, str(day),str(month_options[month][1]),str(year))
+                     % (time.mktime(datetime.datetime(year, month, day).timetuple())*1000, str(day), str(month_options[month][1]), str(year))
                         start_opts = start_opts + option
                     start_opts = start_opts + '</optgroup>'
 
             start_opts = start_opts + '</optgroup>'
         start_opts = start_opts + '</select>'
-        start_ts=time.mktime(start_date.date().timetuple())*1000
-        end_ts=time.mktime(end_date.date().timetuple())*1000
-        start_re=re.compile("<option value=%d>"%start_ts)
-        end_re=re.compile("<option value=%d>"%end_ts)
-        start_selected_str="<option value=%d' selected='selected'>"%start_ts
-        end_selected_str="<option value=%d' selected='selected'>"%end_ts
+        start_ts = time.mktime(start_date.date().timetuple())*1000
+        end_ts = time.mktime(end_date.date().timetuple())*1000
+        start_re = re.compile("<option value=%d>" % start_ts)
+        end_re = re.compile("<option value=%d>" % end_ts)
+        start_selected_str = "<option value=%d' selected='selected'>" % start_ts
+        end_selected_str = "<option value=%d' selected='selected'>" % end_ts
         start_html = start_opts % ('start', '', 'start', 'start')
         end_html = start_opts % ('end', '', 'end', 'end')
-        start_html=start_re.sub(start_selected_str,start_html)
-        end_html=end_re.sub(end_selected_str,end_html)
+        start_html = start_re.sub(start_selected_str, start_html)
+        end_html = end_re.sub(end_selected_str, end_html)
         return mark_safe(start_html + end_html)
 
 
@@ -155,16 +155,16 @@ def do_date_range(parser, token):
 	if not len(chunks) == 5:
 		raise template.TemplateSyntaxError, "%r tag requires two arguments" % token.contents.split()[0]
 
-	return DateRangeNode(chunks[1],chunks[2],chunks[3],chunks[4])
+	return DateRangeNode(chunks[1], chunks[2], chunks[3], chunks[4])
 
 
 register = template.Library()
 register.filter('section', get_section)
 register.filter('parent', get_parent)
 register.filter('parentId', get_parentId)
-register.filter('ancestors',get_ancestors)
+register.filter('ancestors', get_ancestors)
 register.filter('name', name)
 register.filter('latest', latest)
 register.filter('hash', hash)
 register.filter('get_district', get_district)
-register.tag('date_range',do_date_range)
+register.tag('date_range', do_date_range)
